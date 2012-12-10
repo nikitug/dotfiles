e2c80884651c507f60255d6da34ecc5993f47d3f
Nikita Afanasenko <nikita@afanasenko.name>
2012-12-10 13:17:15 +0400

    fix tree availability check

--- system/aliases.bash
+++ system/aliases.bash
@@ -4,7 +4,7 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 
-if [ ! -x "$(which tree)" ]
+if $(which tree &>/dev/null)
 then
   alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
 fi
